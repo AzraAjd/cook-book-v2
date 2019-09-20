cfd9289541367462f8af623e4eee3bc61062fe27
Azra Ajd <azra.ajdinovic@stu.ibu.edu.ba>
2019-09-20 12:32:44 +0200

    Initial few examples

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/*function Events(props) {
+  const clickHandler = console.log;
+  return (<button onClick={clickHandler}>Make an event</button>);
+};*/
+
+/*function Nocheckbox() {
+  return <input type="checkbox" onClick={(e) => {e.preventDefault();}}/>;
+}*/
+
+class Reloader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {content: ""};
+    this.onChar = this.onChar.bind(this);
+    this.onGoTime = this.onGoTime.bind(this);
+  }
+  onChar(event) {
+    this.setState({ content: event.target.value });
+  }
+  onGoTime(event) {
+    if (this.state.content !== "reload") {
+      event.preventDefault();
+    }
+  }
+
+  render() {
+    return (
+     <form onSubmit={this.onGoTime}>
+        <input type="text" value={this.state.content} onChange={this.onChar} />
+        <input type="submit" value="Go Time" />
+     </form>);
+  }
 }
 
-export default App;
+  
+
+
+/*function App() {
+  return (
+    <div className="App">
+      <Events/>
+    </div>
+  );
+}*/
+
+
+ 
+export default Reloader;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Events from './App';
+import Nocheckbox from './App';
 import * as serviceWorker from './serviceWorker';
+import Reloader from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+/*ReactDOM.render(<Events />,
+    document.getElementById('root')
+);*/
+
+/*ReactDOM.render(<Nocheckbox />,
+    document.getElementById('root')
+);*/
+
+ReactDOM.render(<Reloader />, document.getElementById('root')
+);
+
 serviceWorker.unregister();
